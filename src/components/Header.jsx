726420a436d7a44889bef2940924c4fe42be7646
Kevin Donati <91902874+kevindonati@users.noreply.github.com>
2026-07-23 11:51:18 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Container, Nav, Navbar } from "react-bootstrap"
+import { Container, Nav, Navbar, NavDropdown, Image } from "react-bootstrap"
 
 const Header = function () {
   return (
@@ -7,9 +7,24 @@ const Header = function () {
         <Navbar.Brand href="#home">Gestione Energia</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+          <Nav className="w-100 d-flex justify-content-between align-items-center">
+            <div className="d-flex">
+              <Nav.Link href="#">Home</Nav.Link>
+              <Nav.Link href="#">Link</Nav.Link>
+            </div>
+
+            <div className="d-flex">
+              <Image src="Https://placecats.com/45/45" roundedCircle />
+              <NavDropdown id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+              </NavDropdown>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
