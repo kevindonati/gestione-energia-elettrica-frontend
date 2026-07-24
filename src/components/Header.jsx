7d4b5826ff4893b4cc6bd0ad44dc31010d67c801
Kevin Donati <91902874+kevindonati@users.noreply.github.com>
2026-07-24 11:46:47 +0200

    isadmin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,20 @@
-import { Container, Nav, Navbar, NavDropdown, Image } from "react-bootstrap"
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 const Header = function () {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Gestione Energia</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/home">
+          Gestione Energia
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="w-100 d-flex justify-content-between align-items-center">
             <div className="d-flex">
-              <Nav.Link href="#">Home</Nav.Link>
+              <Nav.Link as={Link} to="/home">
+                Home
+              </Nav.Link>
 
               <NavDropdown title="Clienti" id="basic-nav-dropdown2">
                 <NavDropdown.Item as={Link} to="/lista-clienti">
@@ -27,19 +31,6 @@ const Header = function () {
                 </NavDropdown.Item>
                 <NavDropdown.Item as={Link} to="/crea-fattura">
                   Crea fattura
-                </NavDropdown.Item>
-              </NavDropdown>
-            </div>
-
-            <div className="d-flex">
-              <Image src="Https://placecats.com/45/45" roundedCircle />
-              <NavDropdown id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
                 </NavDropdown.Item>
               </NavDropdown>
             </div>
